54f1eddcb647daf190d7dd3a301e8cf588eac350
Kourylenko <Kourylenko@gmail.com>
2019-06-12 21:18:22 +0300

    Create Note Test and project structure

---
+++ src/test/java/NotepadTests.java
@@ -0,0 +1,32 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class NotepadTests {
+
+    WebDriver driver;
+
+    @Before
+    public void openBrowser()
+    {
+        WebDriverManager.chromedriver().setup();
+       driver = new ChromeDriver();
+    }
+
+    @Test
+    public void loadTest()
+    {
+
+    }
+
+
+
+    @After
+    public void closeBrowser()
+    {
+        driver.quit();
+    }
+}
