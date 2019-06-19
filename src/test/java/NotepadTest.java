96b9036d9240b6e663d587ba3c23999751cabdfc
Kourylenko <Kourylenko@gmail.com>
2019-06-19 20:03:19 +0300

    Homework. Added 3 automated tests.

--- src/test/java/NotepadTest.java
+++ src/test/java/NotepadTest.java
@@ -1,21 +1,16 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import io.qameta.allure.junit4.DisplayName;
 import objects.Notepad;
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import static objects.Notepad.*;
-
 public class NotepadTest {
-
 
    WebDriver driver;
    Notepad notePad;
 
-   @Before
+   @BeforeClass
    public void openBrowser()
    {
        WebDriverManager.chromedriver().setup();
@@ -24,20 +19,49 @@ public class NotepadTest {
    }
 
    @Test
-   public void loadTest()
+   @DisplayName("GL 457,460: Create, Save, Delete notes for an unregistered user")
+   public void testCreateAndDeleteNote()
    {
        String title = "My New Note";
        String note = "Hello!";
        notePad.open()
                .setTitle(title)
-               .setContent(note);
-//               .save();
-
+               .setContent(note)
+               .saveNote();
        Assert.assertEquals(title,notePad.getNoteTitle());
        Assert.assertEquals(note,notePad.getNoteContent());
 
-//       notePad.delete();
+       notePad.deleteNote();
    }
+
+    @Test
+    @DisplayName("GL-477:Create a free account")
+    public void testCreateNewAccount()
+    {
+        String email = "Test@gmail.com";
+        String password = "12345";
+        notePad.open()
+                .clickRegisterLoginMenuBtn()
+                .fillEmailFieldRegister(email)
+                .fillPasswordFieldRegister(password);
+        Assert.assertEquals(email,notePad.getEmailRegisterField());
+
+        notePad.clickCreateAccountButton();
+    }
+
+    @Test
+    @DisplayName("GL-496:Successful login in an existing account")
+    public void testLogin()
+    {
+        String login = "Test@gmail.com";
+        String pass = "test";
+        notePad.open()
+                .clickRegisterLoginMenuBtn()
+                .fillEmailFieldLogin(login)
+                .fillPasswordFieldLogin(pass);
+        Assert.assertEquals(login,notePad.getEmailLoginField());
+        notePad.clickLoginButton();
+    }
 
    @After
    public void closeBrowser()
--- src/test/java/objects/Notepad.java
+++ src/test/java/objects/Notepad.java
@@ -1,5 +1,6 @@
 package objects;
 
+import io.qameta.allure.Step;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -8,10 +9,23 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Notepad {
 
-    private static final By noteTitle = By.id("edit_title");
-    private static final By noteContent = By.id("edit_textarea");
-    private static final By saveNoteButton = By.id("btnSaveNote");
-    private static final By deleteNoteButton = By.cssSelector(".delete");
+    //    testCreateAndDeleteNote
+    private static final By noteTitle = By.xpath("//*[@id='edit_title']");
+    private static final By noteContent = By.xpath("//*[@id='edit_textarea']");
+    private static final By saveNoteButton = By.xpath("//*[@id='btnSaveNote']");
+    private static final By deleteNoteButton = By.xpath("//*[@class='delete']");
+
+    //    testCreateNewAccount
+    private static final By registerLoginBtn = By.xpath("//span[contains(text(),'Register/Login')]");
+    private static final By emailRegisterField = By.xpath("//*[@id='registerEmail']");
+    private static final By passwordRegisterField = By.xpath("//input[@placeholder='New Password']");
+    private static final By createAccountButton = By.xpath("//button[contains(text(),'Create Account')]");
+
+    //    testLoginInAccount
+    private static final By emailLoginField = By.xpath("//*[@id='loginEmail']");
+    private static final By passwordLoginField = By.xpath("//input[@placeholder='Enter Password']");
+    private static final By loginButton = By.xpath("//button[contains(text(),'Login')]");
+
 
     private WebDriver driver;
     private WebDriverWait wait;
@@ -22,48 +36,45 @@ public class Notepad {
         this.wait = new WebDriverWait(this.driver, 5);
     }
 
+//    testCreateAndDeleteNote
+
+    @Step
     public Notepad open()
     {
         driver.get("https://anotepad.com/");
         return this;
     }
-
-
+    @Step
     public Notepad setTitle(String title)
     {
         driver.findElement(noteTitle).sendKeys(title);
         return this;
     }
-
-
+    @Step
     public Notepad setContent(String content)
     {
         driver.findElement(noteContent).sendKeys(content);
         return this;
     }
-
-
-    public Notepad save()
+    @Step
+    public Notepad saveNote()
     {
         driver.findElement(saveNoteButton).click();
         wait.until(ExpectedConditions.textToBePresentInElementLocated(By.cssSelector(".alert, alert-warning"), "You have saved your note as a"));
         return  this;
     }
-
-
+    @Step
     public String getNoteTitle()
     {
         return driver.findElement(noteTitle).getAttribute("value");
     }
-
-
+    @Step
     public String getNoteContent()
     {
         return driver.findElement(noteContent).getAttribute("value");
     }
-
-
-    public Notepad delete()
+    @Step
+    public Notepad deleteNote()
     {
         driver.findElement(deleteNoteButton).click();
         Alert alert = wait.until(ExpectedConditions.alertIsPresent());
@@ -72,4 +83,70 @@ public class Notepad {
         return  this;
     }
 
+//    testCreateNewAccount
+
+    @Step
+    public Notepad clickRegisterLoginMenuBtn()
+    {
+        driver.findElement(registerLoginBtn).click();
+        return this;
+    }
+    @Step
+    public Notepad fillEmailFieldRegister(String email)
+    {
+        driver.findElement(emailRegisterField).clear();
+        driver.findElement(emailRegisterField).sendKeys(email);
+        return this;
+    }
+    @Step
+    public Notepad fillPasswordFieldRegister(String password)
+    {
+        driver.findElement(passwordRegisterField).clear();
+        driver.findElement(passwordRegisterField).sendKeys(password);
+        return this;
+    }
+    @Step
+    public String getEmailRegisterField()
+    {
+        return driver.findElement(emailRegisterField).getAttribute("value");
+    }
+    @Step
+    public Notepad clickCreateAccountButton()
+    {
+        driver.findElement(createAccountButton).click();
+        return this;
+    }
+
+//    testLoginInAccount
+
+    @Step
+    public Notepad fillEmailFieldLogin(String login)
+    {
+        driver.findElement(emailLoginField).clear();
+        driver.findElement(emailLoginField).sendKeys(login);
+        return this;
+    }
+    @Step
+    public Notepad fillPasswordFieldLogin(String pass)
+    {
+        driver.findElement(passwordLoginField).clear();
+        driver.findElement(passwordLoginField).sendKeys(pass);
+        return this;
+    }
+    @Step
+    public Notepad clickLoginButton()
+    {
+        driver.findElement(loginButton).click();
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[contains(text(),'Settings')]")));
+        return this;
+    }
+    @Step
+    public String getEmailLoginField()
+    {
+        return driver.findElement(emailLoginField).getAttribute("value");
+    }
+
 }
+
+
+
