83849886d1ec037435b0b30a29f8d814fe3fde48
Kourylenko <Kourylenko@gmail.com>
2019-06-26 23:20:11 +0300

    Add a test "Create a note" on a cucumber

--- src/test/java/objects/StepDefs.java
+++ src/test/java/objects/StepDefs.java
@@ -1,40 +1,93 @@
 package objects;
 
+import com.codeborne.selenide.Condition;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.When;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static com.codeborne.selenide.Selenide.$x;
+import static com.codeborne.selenide.Selenide.open;
+
 
 public class StepDefs {
-
-    private WebDriver driver;
-    private WebDriverWait wait;
-
-    public StepDefs(WebDriver driver)
-    {
-        this.driver = driver;
-        this.wait = new WebDriverWait(this.driver, 5);
-    }
-    @Given("Notepad is opened in Chrome browser")
-    public void notepadIsOpenInChromeBrowser()
-    {
-        driver.get("https://anotepad.com/");
+    @Given("^Notepad is opened in Chrome browser$")
+    public void notepadIsOpenInChromeBrowser() throws Throwable {
+        open("https://anotepad.com/");
     }
 
-    @When("I entered text in Note Title field")
-    public void iEnterTextInNoteTitleField(String noteTitle)
-    {
-        driver.findElement(By.xpath("//*[@id='edit_title']")).sendKeys("Note Title");
+    @When("^I entered text in Note Title field \"([^\"]*)\"$")
+    public void iEnterTextInNoteTitleField(String arg0) throws Throwable {
+        $x("//*[@id='edit_title']").sendKeys(arg0);
     }
 
-    @And("I clicked Save button")
-    public void iClickSaveButton()
-    {
-        driver.findElement(By.xpath("//*[@id='btnSaveNote']")).click();
-        wait.until(ExpectedConditions.textToBePresentInElementLocated(By.cssSelector(".alert.alert-warning"), "You have saved your note as a"));
-
+    @And("^I entered text in Note Content field \"([^\"]*)\"$")
+    public void iEnterTextInNoteContentField(String arg1) throws Throwable {
+        $x("//*[@id='edit_textarea']").sendKeys(arg1);
     }
+
+    @And("^I clicked Save button")
+    public void iClickSaveButton() throws Throwable {
+        $x("//*[@id='btnSaveNote']").click();
+    }
+
+    @And("^I saw message \"You have saved your note as a\"")
+    public void iSawMessage() throws Throwable {
+        $x("//p[@class='alert alert-warning']").shouldHave(Condition.text("You have saved your note as a"));
+    }
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//    @When("^I click on first search results$")
+//    public void iClickOnFirstSearchResults() throws Throwable {
+//        $("h3").click();
+//    }
+//
+//    @Then("^Opened page should contain info about \"([^\"]*)\"$")
+//    public void openedPageShouldContainInfoAbout(String arg0) throws Throwable {
+//        $("div.news div.news-line").shouldHave(Condition.text(arg0));
+//    }
+//
+//    @When("I click on second search results")
+//    public void iClickOnSecondSearchResults() {
+//
+//        $$("h3").get(1).click();
+//
+//    }
+//
+//    @And("Results number is {int}")
+//    public void resultsNumberIs(int arg0) {
+//        $$("h3").shouldHaveSize(arg0);
+//    }
+//}
--- src/test/resources/GherkinTests.feature
+++ src/test/resources/GherkinTests.feature
@@ -1,15 +1,21 @@
-Feature: Create new Note
-  Create New Note in ANotepad
+Feature: Create and delete new Note
+  Create and Delete New Note in ANotepad
 
   Scenario: I create new Note
 
     Given Notepad is opened in Chrome browser
-    When I entered text in Note Title field
+    When I entered text in Note Title field "New Note"
+    And I entered text in Note Content field "Hello"
     And I clicked Save button
-    #Then  Note Title field displayed text "New Note"
-    #When I entered text "Hello!" in Note Content field
-    #Then Note Content field displayed text "Hello!"
+    And I saw message "You have saved your note as a"
 
-    #Then I saw massage "You have saved your note as a"
-    #Then Saved Note displayed in "My Saved Notes" block
+
+  Scenario: I delete Note
+    Given  Notepad is opened in Chrome browser
+    And I have previously created a note
+    When I clicked Delete button
+    Then I saw a question about accepting or dismissing my operation
+    And I clicked Asept
+    And I saw message "No notw here"
+
 
